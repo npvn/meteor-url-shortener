4f314d2fe53c95c55318598c9134920fd5c5e1a5
Phuc Nguyen <mail@nguyenphuc.info>
2014-01-12 16:53:13 +0000

    added fast-render package

---
+++ packages/meteor-fast-render/README.md
@@ -0,0 +1,3 @@
+# meteor-fast-render
+
+[![Meteor Fast Render Documentation](https://i.cloudup.com/PLKYbmCvah.png)](http://meteorhacks.com/fast-render)
---
+++ packages/meteor-fast-render/package.js
@@ -0,0 +1,53 @@
+Package.describe({
+  "summery": "a way to render the initial page super fast!"
+});
+
+Npm.depends({
+  "connect": "2.12.0"
+});
+
+Package.on_use(function(api) {
+  api.use(['minimongo', 'livedata', 'mongo-livedata', 'ejson', 'underscore', 'webapp'], ['server']);
+  api.use(['underscore', 'deps', 'ejson'], ['client']);
+
+  // This is needed due to Meteor Issue #1358
+  //   https://github.com/meteor/meteor/issues/1358
+  // The 'weak' flag doesn't support non-core (ie. atmosphere) packages yet.
+  if(isIronRouterExists()) {
+    api.use(['iron-router'], ['client', 'server'], {weak: true});
+  }
+
+  api.add_files([
+    'lib/server/inject_data.html',
+    'lib/server/inject_config.html',
+  ], 'server', {isAsset: true}); 
+
+  api.add_files([
+    'lib/server/utils.js',
+    'lib/server/fast_render.js',
+    'lib/server/context.js',
+    'lib/server/inject.js',
+    'lib/server/iron_router_support.js',
+  ], 'server');  
+
+  api.add_files([
+    'lib/vendor/cookies.js',
+    'lib/vendor/deepExtend.js',
+    'lib/client/log.js',
+    'lib/client/fast_render.js',
+    'lib/client/ddp_update.js',
+    'lib/client/data_handler.js',
+    'lib/client/iron_router_support.js',
+    'lib/client/auth.js'
+  ], 'client'); 
+
+  api.export('FastRender', ['client', 'server']);
+  api.export('__init_fast_render', ['client']);
+});
+
+function isIronRouterExists() {
+  var fs = Npm.require('fs');
+  var path = Npm.require('path');
+  var meteorPackages = fs.readFileSync(path.resolve('.meteor/packages'), 'utf8');
+  return !!meteorPackages.match(/iron-router/);
+}
