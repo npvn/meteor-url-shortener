a7a8d1b499bbec68a4af02425efc70d66b70e225
Phuc Nguyen <mail@nguyenphuc.info>
2014-01-14 17:08:53 +0000

    bug fix: client can now insert new record to Visits db

--- collections/visits.js
+++ collections/visits.js
@@ -1,2 +1,14 @@
 Visits = new Meteor.Collection('visits');
 
+Visits.allow({
+    insert: function(userId, doc) {
+        console.log(doc);
+        var allowedFields = ['_id', 'shortURL', 'browser', 'os', 'country'];
+
+        for (var prop in doc) {
+            if ( ! _.contains(allowedFields, prop) ) return false;
+        }
+
+        return true;
+    }
+});
