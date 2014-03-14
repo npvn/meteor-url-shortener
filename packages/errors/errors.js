243495ea5e7a72bad6bc7a07ebc151704e1e3e5b
Phuc Nguyen <mail@nguyenphuc.info>
2014-03-14 12:35:28 +0700

    improve packages structure

--- packages/errors/errors.js
+++
@@ -1,9 +0,0 @@
-Errors = {
-	collection: new Meteor.Collection(null),
-	throw: function(message) {
-		Errors.collection.insert({ message: message, seen: false });
-	},
-	clearSeen: function() {
-		Errors.collection.remove({ seen: true });
-	}
-};
--- packages/meteor-fast-render/README.md
+++
@@ -1,3 +0,0 @@
-# meteor-fast-render
-
-[![Meteor Fast Render Documentation](https://i.cloudup.com/PLKYbmCvah.png)](http://meteorhacks.com/fast-render)
