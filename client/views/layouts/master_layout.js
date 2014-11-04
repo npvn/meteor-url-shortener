fbda1514b9afb2c47c40877261a35210ced52dbf
Phuc Nguyen <mail@nguyenphuc.info>
2014-11-04 15:08:43 +0700

    Update snackbar

--- client/views/layouts/master_layout.js
+++
@@ -1,8 +0,0 @@
-Template.MasterLayout.helpers({
-    notification: function () {
-        return Session.get('notification');
-    }
-    , uiClass: function () {
-        return Session.get('show-notification');
-    }
-});
